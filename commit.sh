0c282b4804493743dad3e6da2cfa881dbf4f0564
CBujeda <clarabujedamunoz@gmail.com>
2022-10-25 20:02:57 +0200

    upload

--- commit.sh
+++ commit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd C:\Users\USUARIO\OneDrive - INSPECTORIA SALESIANA MARIA AUXILIADORA\Documentos\Visual studio\SGE\proyecto-IA-Python\IAsge
 git pull
 git add .
 git commit -m "upload"
